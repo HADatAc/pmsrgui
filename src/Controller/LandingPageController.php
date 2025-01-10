9c03d0b9ab88267ae8d261ec34ffec3b647da4d4
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-01-10 16:08:09 +0000

    added images and small fix

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -50,16 +50,18 @@ class LandingPageController extends ControllerBase {
     }
 
     // HTML HEADER
-    $output = '<div id="landing_header" class="row background-portuguese-flag">';
+    $output = '<div id="landing_header" class=" background-portuguese-flag">';
     $output .= '
-      <div class="col-2 d-flex align-items-center">
-        <img class="px-5" height="150" src="'.$logo_url.'" />
-      </div>
-      <div class="col-8 d-flex align-items-center">
-        <h2 class="text-white">Repositório Médico Português</h2>
-      </div>
-      <div class="col-2 d-flex align-items-center text-align-center">
-        ' . $user_links . '
+      <div class="row pt-3">
+        <div class="col d-flex align-items-center">
+          <img class="px-5" height="150" src="'.$logo_url.'" />
+        </div>
+        <div class="col-8 d-flex align-items-center">
+          <h2 class="text-white">Repositório Médico Português</h2>
+        </div>
+        <div class="col d-flex align-items-center text-align-center">
+          ' . $user_links . '
+        </div>
       </div>
     ';
     $output .= '</div>';
@@ -68,19 +70,19 @@ class LandingPageController extends ControllerBase {
     $output .= '<div class="container-image">
                   <div class="row text-center p-0">
                     <div class="col-md-4 p-0">
-                      <img src="https://images.stockcake.com/public/a/b/1/ab1fed88-d3a1-4768-9d47-a9b3e120eff9/high-tech-laboratory-interior-stockcake.jpg" class="custom-img" alt="Image 1">
+                      <img src="'.base_path() . $module_path . '/images/img1.jpg" class="custom-img" alt="Image 1">
                     </div>
                     <div class="col-md-4 p-0">
-                      <img src="https://cdn2.picryl.com/photo/2013/02/20/working-in-laboratories-such-as-this-one-digital-forensics-840865-1024.jpg" class="custom-img" alt="Image 2">
+                      <img src="'.base_path() . $module_path . '/images/img2.jpg" class="custom-img" alt="Image 2">
                     </div>
                     <div class="col-md-4 p-0">
-                      <img src="https://cdn2.picryl.com/photo/2011/02/04/optical-therapeutics-and-medical-nanophotonics-laboratory-5426179192-b63076-1024.jpg" class="custom-img" alt="Image 3">
+                      <img src="'.base_path() . $module_path . '/images/img3.jpg" class="custom-img" alt="Image 3">
                     </div>
                   </div>
                 </div>';
 
     // HTML FOR BUTTONS
-    $output .= '<div class="container text-center my-3">';
+    $output .= '<div class="container text-center my-5">';
     $output .= '<div class="row">';
 
     // Column 1
