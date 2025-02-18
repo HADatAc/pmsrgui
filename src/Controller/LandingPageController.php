2348fb0839565852f0296b46f18e5948afab45c6
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-02-18 16:47:00 +0000

    Updated Links, Buttons and Disabled option

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -73,75 +73,25 @@ class LandingPageController extends ControllerBase {
     // Buttons definition
     $buttons_col1 = [
       ['icon' => 'fas fa-chart-bar fa-2xl', 'label' => 'Manage<br /> Simulator Model', 'url' => 'sir/select/instrument/1/9'],
-      ['icon' => 'fas fa-magnifying-glass fa-2xl', 'label' => 'Search Simulator', 'url' => '#'],
-
+      ['icon' => 'fas fa-magnifying-glass fa-2xl', 'label' => 'Search Simulator<br /> By Hierarchy', 'url' => 'sir/list'],
+      ['icon' => 'fas fa-magnifying-glass fa-2xl', 'label' => 'Search Instances<br /> By Geography', 'url' => '#', 'disabled' => true],
     ];
 
     $buttons_col2 = [
-      ['icon' => 'fas fa-chart-bar fa-2xl', 'label' => 'Manage<br /> Simulator Instances', 'url' => '#'],
-      ['icon' => 'fas fa-magnifying-glass fa-2xl', 'label' => 'Find and Access Data', 'url' => '#'],
+      ['icon' => 'fas fa-chart-bar fa-2xl', 'label' => 'Manage<br /> Simulator Instances', 'url' => 'dpl/select/instrumentinstance/1/9'],
+      ['icon' => 'fas fa-magnifying-glass fa-2xl', 'label' => 'Search Simulator<br /> By Anatomy', 'url' => '#', 'disabled' => true],
+      ['icon' => 'fas fa-magnifying-glass fa-2xl', 'label' => 'Find and Access Data', 'url' => '#', 'disabled' => true],
 
     ];
 
     $buttons_col3 = [
-      ['icon' => 'fas fa-chart-bar fa-2xl', 'label' => 'Manage Use Cases', 'url' => '#'],
-      ['icon' => 'fas fa-magnifying-glass fa-2xl', 'label' => 'Social Search', 'url' => '#'],
+      ['icon' => 'fas fa-chart-bar fa-2xl', 'label' => 'Manage Use Cases', 'url' => '#', 'disabled' => true],
+      ['icon' => 'fas fa-magnifying-glass fa-2xl', 'label' => 'Search Organization<br /> By Geography', 'url' => 'sir/list', 'disabled' => true],
+      ['icon' => 'fas fa-magnifying-glass fa-2xl', 'label' => 'Search<br /> People by Geography', 'url' => '#', 'disabled' => true],
     ];
-
-    // Check if user is authenticated
-    // $current_user = \Drupal::currentUser();
-    // if ($current_user->isAuthenticated()) {
-    //   // User authenticated: show Profile and Log Out options
-    //   $user_profile_url = Url::fromRoute('entity.user.canonical', ['user' => $current_user->id()])->toString();
-    //   $logout_url = Url::fromRoute('user.logout')->toString();
-    //   $user_links = '
-    //     <a class="nav-link text-white" href="' . $user_profile_url . '">Profile</a>&nbsp;|&nbsp;
-    //     <a class="nav-link text-white" href="' . $logout_url . '">Log Out</a>
-    //   ';
-    // } else {
-    //   // Anonymous user: show Login and Sign Up options
-    //   $login_url = Url::fromRoute('user.login')->toString();
-    //   $signup_url = Url::fromRoute('user.register')->toString();
-    //   $user_links = '
-    //     <a class="nav-link text-white" href="' . $login_url . '?destination=pmsr">Login</a>&nbsp;|&nbsp;
-    //     <a class="nav-link text-white" href="' . $signup_url . '?destination=pmsr">Sign Up</a>
-    //   ';
-    // }
 
     // INIT HTML
     $output = '';
-
-    // HTML HEADER
-    // $output = '<div id="landing_header" class=" background-portuguese-flag">';
-    // $output .= '
-    //   <div class="row pt-3">
-    //     <div class="col d-flex align-items-center">
-    //       <img class="px-5" height="150" src="'.$logo_url.'" />
-    //     </div>
-    //     <div class="col-8 d-flex align-items-center">
-    //       <h1 class="text-black">'.$title.'</h1>
-    //     </div>
-    //     <div class="col d-flex align-items-center text-align-center text-white">
-    //       ' . $user_links . '
-    //     </div>
-    //   </div>
-    // ';
-    // $output .= '</div>';
-
-    // // HTML IMAGES ROW
-    // $output .= '<div class="container-image">
-    //               <div class="row text-center p-0">
-    //                 <div class="col-md-4 p-0">
-    //                   <img src="'.$img_1.'" class="custom-img" alt="Image 1">
-    //                 </div>
-    //                 <div class="col-md-4 p-0">
-    //                   <img src="'.$img_2.'" class="custom-img" alt="Image 2">
-    //                 </div>
-    //                 <div class="col-md-4 p-0">
-    //                   <img src="'.$img_3.'" class="custom-img" alt="Image 3">
-    //                 </div>
-    //               </div>
-    //             </div>';
 
     // HTML FOR BUTTONS
     $output .= '<div class="container my-5">';
@@ -151,32 +101,68 @@ class LandingPageController extends ControllerBase {
     if ($user->isAuthenticated()) {
       // Column 1
       $output .= '<div class="col-4 d-flex flex-column justify-content-between">';
+      // For each button, decide if it's disabled.
       foreach ($buttons_col1 as $button) {
-        $output .= '<a href="' . $button['url'] . '" class="btn btn-primary btn-lg my-2 d-flex align-items-center justify-content-center custom-button">';
-        $output .= '<i class="' . $button['icon'] . ' me-2"></i>&nbsp;';
-        $output .= '<h5>' . $button['label'] . '</h5>';
+        // Base classes for styling
+        $classes = 'btn btn-primary btn-lg my-2 d-flex align-items-center justify-content-center custom-button';
+
+        // If button['disabled'] is set, add our disabled-link class and set href="#"
+        if (!empty($button['disabled'])) {
+          $classes .= ' disabled-link'; // so pointer-events: none
+          $href = '#'; // or "javascript:void(0)"
+        } else {
+          $href = $button['url'];
+        }
+
+        $output .= '<a href="' . $href . '" class="' . $classes . '">';
+        $output .= '<i class="' . $button['icon'] . ' me-2"></i>&nbsp;<h5>' . $button['label'] . '</h5>';
         $output .= '</a>';
       }
+
       $output .= '</div>';
 
       // Column 2
       $output .= '<div class="col-4 d-flex flex-column justify-content-between">';
+      // For each button, decide if it's disabled.
       foreach ($buttons_col2 as $button) {
-        $output .= '<a href="' . $button['url'] . '" class="btn btn-primary btn-lg my-2 d-flex align-items-center justify-content-center custom-button">';
-        $output .= '<i class="' . $button['icon'] . ' me-2"></i>&nbsp;';
-        $output .= '<h5>' . $button['label'] . '</h5>';
+        // Base classes for styling
+        $classes = 'btn btn-primary btn-lg my-2 d-flex align-items-center justify-content-center custom-button';
+
+        // If button['disabled'] is set, add our disabled-link class and set href="#"
+        if (!empty($button['disabled'])) {
+          $classes .= ' disabled-link'; // so pointer-events: none
+          $href = '#'; // or "javascript:void(0)"
+        } else {
+          $href = $button['url'];
+        }
+
+        $output .= '<a href="' . $href . '" class="' . $classes . '">';
+        $output .= '<i class="' . $button['icon'] . ' me-2"></i>&nbsp;<h5>' . $button['label'] . '</h5>';
         $output .= '</a>';
       }
+
       $output .= '</div>';
 
       // Column 3
       $output .= '<div class="col-4 d-flex flex-column justify-content-between">';
+      // For each button, decide if it's disabled.
       foreach ($buttons_col3 as $button) {
-        $output .= '<a href="' . $button['url'] . '" class="btn btn-primary btn-lg my-2 d-flex align-items-center justify-content-center custom-button">';
-        $output .= '<i class="' . $button['icon'] . ' me-2"></i>&nbsp;';
-        $output .= '<h5>' . $button['label'] . '</h5>';
+        // Base classes for styling
+        $classes = 'btn btn-primary btn-lg my-2 d-flex align-items-center justify-content-center custom-button';
+
+        // If button['disabled'] is set, add our disabled-link class and set href="#"
+        if (!empty($button['disabled'])) {
+          $classes .= ' disabled-link'; // so pointer-events: none
+          $href = '#'; // or "javascript:void(0)"
+        } else {
+          $href = $button['url'];
+        }
+
+        $output .= '<a href="' . $href . '" class="' . $classes . '">';
+        $output .= '<i class="' . $button['icon'] . ' me-2"></i>&nbsp;<h5>' . $button['label'] . '</h5>';
         $output .= '</a>';
       }
+
       $output .= '</div>';
     } else {
       // USER IS NOT AUTHENTICATED
